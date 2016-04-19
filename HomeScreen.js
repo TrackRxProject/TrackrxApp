1cb022792dd07ff4cfd9828059c28363a31e14ae
Everett Berry <epberry@purdue.edu>
2016-04-18 21:25:15 -0400

    notification framework + app deployed on device

--- HomeScreen.js
+++ HomeScreen.js
@@ -34,7 +34,7 @@ class HomeScreen extends Component {
           </Text>
         </View>
 
-        <NavButton {...this.props} buttonTitle="Pill Notification" nextScreen="PillNotify"/>
+        <NavButton {...this.props} buttonTitle="Larew Notification" nextScreen="PillNotify"/>
         <NavButton {...this.props} buttonTitle="Adherence History" nextScreen="AdherenceScreen"/>
         <NavButton {...this.props} buttonTitle="Activate Bottle" nextScreen="ActivateScreen"/>
         <NavButton {...this.props} buttonTitle="Select Bottle" nextScreen="SelectScreen"/>
